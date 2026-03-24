9231173e47bf9eb02605badd32b079d7f467a872
mykyta <mykyta.sidorov@kse.org.ua>
2026-03-24 19:16:39 +0200

    Made program.cs with realization

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,11 +1,21 @@
-﻿using System;
-
-namespace Week8;
+﻿namespace Week8;
+using System;
 
 class Program
 {
     static void Main()
     {
-        
+        Letter letter1 = new Letter("322", 0.5);
+        Parcel parcel1 = new Parcel("67", 15.5, "40x25x60");
+
+        letter1.PrintInfo();
+        parcel1.PrintInfo();
+
+        CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+
+        myCargo.AddItem(letter1);
+        myCargo.AddItem(parcel1);
+
+        Console.WriteLine("Повна вартість: " + myCargo.GetTotalCost() + "грн.");
     }
 }
